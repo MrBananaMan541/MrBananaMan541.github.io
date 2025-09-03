e23735be2442bb28226903a119f7201c9aa47064
MrBananaMan541 <mrbananaman541@gmail.com>
2025-09-03 15:41:21 -0400

    Added ability for the site to get the current year whenever it loads

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let currentYear = new Date().getFullYear();
+
 export const init = () => {
-	
+	//Set the copyright year to the current year everytime the page is loaded
+    document.querySelector("footer").innerHTML = `Copyright © ${currentYear} Peter Buechi: Portfolio - All Rights Reserved.`;
 };
