e322616430f5fb09d44b1302b9c1b850e98b91ed
MrBananaMan541 <pab1029@rit.edu>
2025-09-26 10:53:12 -0400

    Started work on image carousel

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@
 
 let currentYear = new Date().getFullYear();
 
+let img = document.querySelector("#placeholder");
+let images = document.querySelectorAll("span[data-image]");
+
+for (let image of images) {
+    image.onclick = function (e) {
+        let imageSrc = e.target.dataset.image;
+        document.querySelector("#description").innerHTML = `You are looking at ${e.target.innerHTML}.`;
+        document.querySelector("#description").style.fontSize = "large";
+        img.src = `images/${imageSrc}`;
+
+        image.style.color = "black";
+        image.style.fontSize = "large";
+        image.style.textDecoration = "underline";
+    };
+}
+
 export const init = () => {
-	//Set the copyright year to the current year everytime the page is loaded
+    //Set the copyright year to the current year everytime the page is loaded
     document.querySelector("footer").innerHTML = `Copyright © ${currentYear} Peter Buechi: Portfolio - All Rights Reserved.`;
 };
