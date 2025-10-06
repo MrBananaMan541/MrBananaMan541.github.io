fcb51df32819c6346e54288f9ad95c884557fc14
MrBananaMan541 <mrbananaman541@gmail.com>
2025-10-06 13:58:37 -0400

    Image carousel is complete, just need previous and next buttons, and then images for skills
    The image carousel is complete, including clicking an image and it both highlighting and switching. It just needs easy buttons to go to the previous and next image, and then 2 images for the skills section

--- src/main.js
+++ src/main.js
@@ -1,20 +1,57 @@
 "use strict";
 
+//Gets a reference to the current year to display
 let currentYear = new Date().getFullYear();
 
+//References to the current image displayed and the list of images to choose from
 let image = document.querySelector("#placeholder");
-let listOfImages = document.querySelectorAll(".imagesInCarousel");
+let shownImages = document.querySelectorAll(".imagesInCarousel");
+let currentSelectedImage = document.querySelector(".imagesInCarousel");
 
-for (let currentImage of listOfImages) {
-    currentImage.onclick = function (e) {
-        let imageSrc = e.target.src;
-        // document.querySelector("#description").innerHTML = `You are looking at ${e.target.innerHTML}.`;
-        // document.querySelector("#description").style.fontSize = "large";
-        image.src = imageSrc;
+//Deltatime to control the carousel rotating automatically
+let dt = 0;
+
+//Sets up the functionality to click on each image
+for (let currentImage of shownImages) {
+    currentImage.onclick = e => nextCarouselImage(e.target);
+}
+
+function nextCarouselImage(newImage) {
+    //Reset deltatime
+    dt = 0;
+    
+    //Set the current "selected" image back to its original state, so it is no longer highlighted
+    currentSelectedImage.src = image.src;
+    //Change the main image to the selected one
+    image.src = newImage.src;
+    //Update the current selected image to the new one that was clicked, and highlight it
+    currentSelectedImage = newImage;
+    currentSelectedImage.src = `${String(newImage.src).split(".png")[0]}-Selected.png`;
+}
+
+function carouselLoop() {
+    //Calculate "delta time"
+    dt += 0.001;
+
+    //If 5 seconds have passed, then move to the next image
+    if (dt >= 1)  {
+        //If the current image is the last one, and the next one is null, loop back to the first one
+        if(currentSelectedImage.nextElementSibling == null) {
+            nextCarouselImage(document.querySelector(".imagesInCarousel"));
+        }
+        //Otherwise continue to the next image
+        else {
+            nextCarouselImage(currentSelectedImage.nextElementSibling);
+        }
     };
+
+    console.log(dt);
+    carouselLoop();
 }
 
 export const init = () => {
     //Set the copyright year to the current year everytime the page is loaded
     document.querySelector("footer").innerHTML = `Copyright © ${currentYear} Peter Buechi: Portfolio - All Rights Reserved.`;
+
+    carouselLoop();
 };
