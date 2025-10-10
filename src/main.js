bb230013a8ec70464c570d84b59a3421347732de
MrBananaMan541 <mrbananaman541@gmail.com>
2025-10-10 13:56:48 -0400

    Finished prev and next buttons for image carousel, and added c# image to skills and credit to it

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ let image = document.querySelector("#placeholder");
 let shownImages = document.querySelectorAll(".imagesInCarousel");
 let currentSelectedImage = document.querySelector(".imagesInCarousel");
 
+let prevCarouselButton = document.querySelector("#prev");
+let nextCarouselButton = document.querySelector("#next");
+
 //Deltatime to control the carousel rotating automatically
 let dt = 0;
 
@@ -17,9 +20,6 @@ for (let currentImage of shownImages) {
 }
 
 function nextCarouselImage(newImage) {
-    //Reset deltatime
-    dt = 0;
-    
     //Set the current "selected" image back to its original state, so it is no longer highlighted
     currentSelectedImage.src = image.src;
     //Change the main image to the selected one
@@ -35,6 +35,9 @@ function carouselLoop() {
 
     //If 5 seconds have passed, then move to the next image
     if (dt >= 1)  {
+        //Reset deltatime
+        dt = 0;
+
         //If the current image is the last one, and the next one is null, loop back to the first one
         if(currentSelectedImage.nextElementSibling == null) {
             nextCarouselImage(document.querySelector(".imagesInCarousel"));
@@ -53,5 +56,28 @@ export const init = () => {
     //Set the copyright year to the current year everytime the page is loaded
     document.querySelector("footer").innerHTML = `Copyright © ${currentYear} Peter Buechi: Portfolio - All Rights Reserved.`;
 
-    carouselLoop();
+    //Set up functionality for the previous and next buttons in the image carousel
+    prevCarouselButton.addEventListener('click', () => {
+        //If the current image is the first one, and the previous one is null, loop to the last one
+        if(currentSelectedImage.previousElementSibling == null) {
+            nextCarouselImage(document.querySelectorAll(".imagesInCarousel")[document.querySelectorAll(".imagesInCarousel").length - 1]);
+        }
+        //Otherwise continue to the previous image
+        else {
+            nextCarouselImage(currentSelectedImage.previousElementSibling);
+        }
+    });
+    nextCarouselButton.addEventListener('click', () => {
+        //If the current image is the last one, and the next one is null, loop back to the first one
+        if(currentSelectedImage.nextElementSibling == null) {
+            nextCarouselImage(document.querySelector(".imagesInCarousel"));
+        }
+        //Otherwise continue to the next image
+        else {
+            nextCarouselImage(currentSelectedImage.nextElementSibling);
+        }
+    });
+
+    //Commented out for now as the timing is way off and results in a stack overflow
+    //carouselLoop();
 };
